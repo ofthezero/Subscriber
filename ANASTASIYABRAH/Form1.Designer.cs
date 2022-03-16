d58105f16b24072a5b7289a493aadc1570c70adf
ofthezero <98045454+ofthezero@users.noreply.github.com>
2022-03-16 14:43:34 +0300

    Добавьте файлы проекта.

---
+++ ANASTASIYABRAH/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace ANASTASIYABRAH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(13, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Message";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(385, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 37);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "00:00:00";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(-13, 111);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(712, 147);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(5, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(615, 103);
+            this.label4.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(71, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 26);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Подключить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(223, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 17);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "IP";
+            this.label6.Click += new System.EventHandler(this.label6_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(399, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 17);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Topic";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ANASTASIYABRAH.Properties.Settings.Default, "Topic", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox3.Location = new System.Drawing.Point(441, 49);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 18;
+            this.textBox3.Text = global::ANASTASIYABRAH.Properties.Settings.Default.Topic;
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ANASTASIYABRAH.Properties.Settings.Default, "IP", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox2.Location = new System.Drawing.Point(251, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(101, 23);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = global::ANASTASIYABRAH.Properties.Settings.Default.IP;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(230, -2);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 39);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Subscriber";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(625, 302);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Subscriber";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ANASTASIYABRAH/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ANASTASIYABRAH
+{
+    public partial class Form1 : Form
+    {
+
+        MqttClient mqttClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer2.Start();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //    timer1.Interval = 5000;
+            //    try
+            //    {
+            //        timer1.Interval = 5000;
+            //        Task.Run(() =>
+
+            //        {
+            //            mqttClient = new MqttClient(textBox2.Text);
+            //            mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+            //            mqttClient.Subscribe(new string[] { textBox3.Text }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            //            mqttClient.Connect(textBox3.Text);
+            //        });
+            //    }
+            //    catch
+            //    {
+            //        MessageBox.Show("Error");
+            //    }
+
+
+        }
+        //private void MqttClient_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        //{
+        //    var message = Encoding.UTF8.GetString(e.Message);
+        //    //listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add(message)));            
+        //    label5.Invoke((MethodInvoker)delegate
+        //    {
+        //        label5.Text = message;
+        //    });
+        //}
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    //timer1.Interval = 1000;           
+        //    //Task.Run(() =>
+        //    //{
+        //    //    if (mqttClient != null && mqttClient.IsConnected)
+        //    //    {
+        //    mqttClient.Publish(textBox3.Text, Encoding.UTF8.GetBytes(textBox1.Text + "  " + label3.Text));
+        //    //    }
+        //    //});            
+        //}
+
+        //public void timer1_Tick(object sender, EventArgs e)
+        //{
+        //    button1.PerformClick();
+        //    //label3.Text = DateTime.Now.ToString();
+        //    button2.PerformClick();
+
+        //}
+
+        private void button2_Click(object sender, EventArgs a)
+        {
+
+        }
+
+        private void MqttClient_MqttMsgPublishReceived1(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs a)
+        {
+            try
+            {
+                var message = Encoding.UTF8.GetString(a.Message);
+                //listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add(message)));
+                label4.Invoke((MethodInvoker)delegate
+                {
+                    label4.Text = message;
+                });
+            }
+            catch { }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try { 
+            Ping ping = new Ping();
+            PingReply reply = ping.Send(textBox2.Text, 1000);
+            MessageBox.Show(reply.Status.ToString());
+            timer1.Interval = 1000;
+            try
+            {
+                timer1.Interval = 1000;
+                Task.Run(() =>
+                {
+                    mqttClient = new MqttClient(textBox2.Text);
+                    mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived1;
+                    mqttClient.Subscribe(new string[] { textBox3.Text }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+                    mqttClient.Connect("324");
+                });
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        //private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    if (checkBox1.Checked == true)
+        //    {
+        //        timer1.Start();
+        //        timer1.Interval = 1000;
+        //    }
+        //    else
+        //    {
+        //        timer1.Stop();
+        //    }
+        //}
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label3.Text = DateTime.Now.ToString();
+        }
+
+        private void label6_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Properties.Settings.Default.CountryId = comboBox1.SelectedValue.ToString();
+            Properties.Settings.Default.Save();
+        }
+    }
+}
+
+//ANASTASIYABRAH
